fce103d21ffdf244e319fc2a8d47e2088681b639
Hellrazers <88717988+Hellrazers@users.noreply.github.com>
2025-12-20 15:05:29 +0200

    Add files via upload

---
+++ Lessons/lesson_1/main.py
@@ -0,0 +1,97 @@
+55 == 55
+"Alex" == "alex"
+55 == "55"
+55 == 55.0
+
+my_name_is = True #boolean
+my_name = "Oleksii"
+my_age = 28 # int
+my_temperature = 36.6 #floot
+
+#
+# cats_ages = [3, 4] # list
+# my_dict = {"name" : "oleskii", "surnem ": "123"}
+# my_set ={ 3, 5} #set
+# None #none
+sum_1 = my_age + my_temperature
+print(my_age)
+min_1 =   my_temperature -my_age
+print(min_1)
+div_1 = my_age / my_temperature
+print(div_1)
+multy_1 = my_age * my_temperature
+print(multy_1)
+print(round(min_1))
+some_value = f'мене звуть {my_name}, мій вік {my_age}'
+print("моє ім'я", my_name, "мій вік", my_age)
+print(some_value)
+
+#
+# print(33+ 44)
+# print(44-11)
+# print(int(20/5))
+# print(5*5)
+# print(int(5.5) + int(5.5))
+#
+# if my_age == 28:
+#     print('мені 28')
+#
+# print('done')
+#
+# MAX_AGE = 28
+#
+# my_age = 30
+# result_sum = my_age // MAX_AGE
+#
+# class Age:
+#     pass
+
+
+
+
+# print((20//5))
+# home_1 = 1
+# home_1_home = 2
+# asd = "12312423543"
+# """
+# У саду посадили 4 яблуні. Груш на 5 більше яблунь, а слив - на 2 менше.
+# Скільки всього дерев посадили в саду?
+# """
+# apple = 4
+# peer = apple + 5
+# var_3 = apple - 2
+# sum_tree = apple + peer + var_3
+# print(f'Яблук - {apple}, Груші - {peer}, Сливи - {apple - 2}')
+# print("Яблук", apple,'Груші', peer,"Сливи", var_3)
+# print("Всього дерев посадили ",  apple + peer + var_3)
+# print(f'some str value  ==== {my_name}')
+# # path user id
+# print(1, end=' ')
+# print(2)
+# item_id = 123
+# path_user_id = f'api/v1/users/{item_id}'
+# print(f'my name is my name ')
+# a = 2
+# b = 3
+# c = a + b / 2
+# my_name = "Oleksii"
+for letter in my_name:
+    print(letter)
+# if my_age == 28:
+#     print('мені 28')
+
+
+
+some_str = "123"
+some_value = 123
+some_value_2 = some_value
+print('some_value', id(some_value))
+some_value = some_value + 1
+print('some_value + 1', id(some_value))
+# print('some_value', id(some_value_2))
+list_values = [1, 2, 3, 4, 5]
+# print('list_values',id(list_values))
+# list_values.append(6)
+# print("list_values after update",id(list_values))
+# print(id(list_values[4]))
+
