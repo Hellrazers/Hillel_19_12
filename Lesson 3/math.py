03b4ea62333a9ceab4a87b404847fc6d9b4d488c
Hellrazers <leshamalakhov@gmail.com>
2025-12-26 23:01:56 +0200

    add lesson 3

---
+++ Lesson 3/math.py
@@ -0,0 +1,40 @@
+# print(5/2) #2.5
+# print(5//2) #2
+# # print(30/0)
+# try:
+#     print(30/0)
+# except ZeroDivisionError:
+#     print("Division by zero is not allowed")
+#
+#
+# print(pow(2, 3))
+# print(2**3)
+#
+# list = (1, 2 , 3)
+# print(min(list))
+# print(max(list))
+
+
+# my_name = input("Enter your name: ")
+# print(f'My name is {my_name} {{a}}')
+# my_age = int(input("Enter your age: "))
+# print(f'My age is {my_age - 1}\n123123')
+# print(110%25)
+# coount = 10
+# assert coount == (110%25)
+# 4 + 1
+# 4/2 + 1 / 2
+list_list = [1, 2, 3, 5, 'str', 'str'] #len(list_list) == 6
+print(list_list)
+set_list = set(list_list)
+print(set_list)
+# assert len(set_list) == len(list_list)
+list_2 = tuple(set_list) #len(list_list) == 5
+for element  in list_2:
+    print('ELEMENT of SET is : ', element)
+    assert element == list_list[list_2.index(element)]
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
+my_tuple = (1, 2)
+my_tuple = (1, 2, 3)
---
+++ Lesson 3/string.py
@@ -0,0 +1,77 @@
+# # frist_sting_1 = 'Це\tодинарний рядок. \n new ro\'w'
+# # frist_sting_2 = "Це подвійний рядок."
+# # row_3_sting_1 = '''Це
+# # трьохкратний ' sad""" as da
+# # рядок.'''
+# # print(row_3_sting_1)
+# # +       -       *       **      /       //      %      @
+# len_list_of_computer = 30
+# b = 2
+# # print(a ** b )
+# # print(b / a ) # 2 + 1/2
+# # print(a % b )
+# # if (len_list_of_computer % b == 0):
+# #     print('True')
+# some_text = len("some text")
+# if some_text < 15:
+#     print('True')
+# if (some_text := len("some text")) < 15:
+#     print('True')
+#
+# # <       >       <=      >=      ==      !=
+# a = 15
+# b = 10
+# print(15 > 10) #True
+# print(15 < 10) #False
+# print(10 <= 10) #True
+# print(10 >= 10) #True
+# print(10 == 10) #True
+# print(9 != 10) #True
+#
+# print(True and True) # True
+# print(True and False) # False
+#
+# print(True or True) # True
+# print(True or False) # True
+# print("False or False: ",(False or False)) # False
+#
+# print( True and (not False)) #False
+#
+# respose_answer = None
+# if respose_answer is None:
+#     print('True')
+#
+# (       )       [       ]       {       }
+print('done')
+my_tuple = (1, 2, 3)
+def my_fuc(my_tuple):
+    print(my_tuple[2])
+c = 5 + (4 * 3)
+# print(True or (False and True) or True) #TRUE | True | True
+my_list = [1, 2, 3]
+print(my_list[1])
+print(my_list[-2])
+print(f'{my_list[1]} is 1 ')
+my_set = {1, 2, 3, 3, 2, 5, 'str', 'str'}
+print(my_set)
+my_dict = {'name': 'Oleksii', 'my_list': my_list}
+print(my_dict['my_list'])
+#,       :       .       ;       @
+my_name: str = 'Oleksii'
+my_name.upper()
+def my_func(amount, value: int) -> int:
+    # # print(amount.upper())
+    # amount.
+    return len(amount) * value
+
+print(my_func('Hello word!', 3.2))
+value_1 = 3.2
+print(my_name.upper())
+# value_of_list_vlauses_int3 = 3; value_4 = 4
+value_3, value_4 = 3, 4
+print(value_3, value_4)
+
+assert (3.3 - 2.0) == 1.3
+dict_name = {"name": "Oleksii", "surname": "123", "age": 28}
+print(dict_name["age"]
+)
