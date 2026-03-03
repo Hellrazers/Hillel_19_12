16523a724e5d81f46f529cb2edbf09df4dc67510
Olekii <leshamalakhov@gmail.com>
2026-03-03 22:09:25 +0200

    add lesson 19

---
+++ lib/models/cars/response_car_models.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from lib.response_model import ResponseModel
+
+
+class CarsModel:
+    id: str
+    carBrandId:str
+    carModelId:str
+
+
+    def __init__(self, data: dict):
+        self.id = data.get("id")
+        self.carBrandId = data.get("carBrandId")
+        self.carModelId = data.get("carModelId")
+
+
+
+class CarResponseModel(ResponseModel):
+    status_code: int
+    response_status: str
+    errors: Any
+    payload: Any
+    resp_obj: Any
+    data: CarsModel = None
+
+    def __init__(self, resp: ResponseModel):
+        self.status_code = resp.status_code
+        self.response_status = resp.response_status
+        self.errors = resp.error
+        self.payload = resp.payload
+        self.resp_obj = resp.resp_obj
+        if resp.data != None:
+            self.data = CarsModel(resp.data)
